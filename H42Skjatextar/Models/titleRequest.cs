bbdf5f2961599739bacc188e8d2e09c723ebeae9
benedikt <benedikthth@gmail.com>
2014-05-14 04:44:16 +0000

    klasar!

---
+++ H42Skjatextar/Models/titleRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace H42Skjatextar.Models
+{
+    public class titleRequest
+    {
+        public int id;
+        public string name;
+        public int year;
+        public string director;
+        public string type;
+        public string genre;
+    }
+}
