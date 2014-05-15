ff15abc8d98621b4b1b56455113981934b75dceb
ArniBodvar <arnib10@ru.is>
2014-05-15 15:11:17 +0000

    TitleRequest class needed primary key

--- H42Skjatextar/Models/titleRequest.cs
+++ H42Skjatextar/Models/titleRequest.cs
@@ -7,7 +7,7 @@ namespace H42Skjatextar.Models
 {
     public class TitleRequest
     {
-        public int requestId { get; set; }
+        public int Id { get; set; }
         public string name { get; set; }
         public int year { get; set; }
         public string director { get; set; }
